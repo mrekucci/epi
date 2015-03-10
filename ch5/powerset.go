3258ca1d027af79c242608a65488a5a023df1c77
Peter Mrekaj <mrekucci@gmail.com>
2015-03-10 12:39:54 +0100

    Solve the "Generate the power set" problem

---
+++ ch5/powerset.go
@@ -0,0 +1,26 @@
+package ch5
+
+import "math"
+
+// powerSet returns a power set of s.
+// The length of s is limited to size of int.
+// When is crossed then nil interface and false is returned.
+func powerSet(s []interface{}) ([]interface{}, bool) {
+	var ps []interface{}
+	if len(s) >= (32 << (^uint(0) >> 63)) {
+		return ps, false
+	}
+
+	for i := 0; i < (1 << uint(len(s))); i++ {
+		x := i
+		var ss []interface{}
+		for x > 0 {
+			bp := int(math.Log2(float64(x & ^(x - 1)))) // Compute least significant bit position of int.
+			ss = append(ss, s[bp])
+			x &= (x - 1) // This ensure that the iteration count will be the same as number of 1 bits in x.
+			// Add if statement x == 0 which will execute on sub-set end.
+		}
+		ps = append(ps, ss)
+	}
+	return ps, true
+}
---
+++ ch5/powerset_test.go
@@ -0,0 +1,68 @@
+package ch5
+
+import (
+	"reflect"
+	"testing"
+)
+
+var powerSetTests = []struct {
+	in   []interface{}
+	want []interface{}
+	ok   bool
+}{
+	{
+		[]interface{}{"A", "B"},
+		[]interface{}{
+			[]interface{}(nil),
+			[]interface{}{"A"},
+			[]interface{}{"B"},
+			[]interface{}{"A", "B"}},
+		true,
+	},
+	{
+		[]interface{}{"A", "B", "C"},
+		[]interface{}{
+			[]interface{}(nil),
+			[]interface{}{"A"},
+			[]interface{}{"B"},
+			[]interface{}{"A", "B"},
+			[]interface{}{"C"},
+			[]interface{}{"A", "C"},
+			[]interface{}{"B", "C"},
+			[]interface{}{"A", "B", "C"}},
+		true,
+	},
+	{
+		[]interface{}(nil), // Will be initialized in init function to 32 or 64 elements according to architecture.
+		[]interface{}(nil),
+		false,
+	},
+}
+
+func init() {
+	for i := range powerSetTests {
+		test := &powerSetTests[i]
+		if !test.ok {
+			l := (32 << (^uint(0) >> 63))
+			for i := 0; i < l; i++ {
+				test.in = append(test.in, "0")
+			}
+		}
+	}
+}
+
+func TestPowerSet(t *testing.T) {
+	for _, tt := range powerSetTests {
+		got, ok := powerSet(tt.in)
+		if !reflect.DeepEqual(got, tt.want) || ok != tt.ok {
+			t.Errorf("powerSet(%#v) = %#v, %t; want %#v, %t", tt.in, got, ok, tt.want, tt.ok)
+		}
+	}
+}
+
+func BenchmarkPowerSet(b *testing.B) {
+	s := []interface{}{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"}
+	for i := 0; i < b.N; i++ {
+		powerSet(s)
+	}
+}
