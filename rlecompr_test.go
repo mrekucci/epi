f64213d57fd5b2b59501cccfab5e0b1096abfa91
Peter Mrekaj <mrekucci@gmail.com>
2015-07-27 20:47:02 -0700

    Add missing copyright to source files

--- rlecompr_test.go
+++ rlecompr_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2015, Peter Mrekaj. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE.txt file.
+
 package epi
 
 import (
