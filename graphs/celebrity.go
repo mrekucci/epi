2441f4c8b17aa535b2624e4803190c310f4f4d51
Peter Mrekaj <mrekucci@gmail.com>
2015-12-02 21:07:07 -0800

    Fix the comment of why column starts at 1

--- graphs/celebrity.go
+++ graphs/celebrity.go
@@ -9,7 +9,7 @@ package graphs
 // doesn't exists in f or if a person exists who doesn't know the celebrity.
 // The time complexity is O(n), and O(1) additional space is needed.
 func FindCelebrity(f [][]bool) int {
-	r, c := 0, 1 // c starts at 1 'cause that's start of diagonal where A<->A, B<->B, C<->C, ...
+	r, c := 0, 1 // c starts at 1 'cause it's after start of diagonal (A<->A, B<->B, C<->C, ...)
 	for c < len(f) {
 		if f[r][c] {
 			r, c = c, c+1 // All candidates less then c are not celebrity candidates.
