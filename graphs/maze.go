59ebbbf2e568231a48aa48920c41397c3a6158e0
Peter Mrekaj <mrekucci@gmail.com>
2015-12-08 20:18:47 -0800

    Rename variables in graphs.SearchMaze function
    This change makes code more readable.

--- graphs/maze.go
+++ graphs/maze.go
@@ -9,19 +9,19 @@ func isFeasible(maze [][]bool, c Coordinate) bool {
 	return c.x >= 0 && c.x < len(maze) && c.y >= 0 && c.y < len(maze[c.x]) && maze[c.x][c.y] == white
 }
 
-// pathExists returns true if path from c to e exists in maze.
+// pathExists returns true if path from curr to end exists in maze.
 // Function is recursive implementation of DFS.
-func pathExists(maze [][]bool, c, e Coordinate, path *[]Coordinate) bool {
-	if c == e { // Base case.
+func pathExists(maze [][]bool, curr, end Coordinate, path *[]Coordinate) bool {
+	if curr == end { // Base case.
 		return true
 	}
 
-	for _, s := range []Coordinate{right, left, down, up} {
-		n := Coordinate{c.x + s.x, c.y + s.y}
-		if isFeasible(maze, n) { // Check if we can move this way.
-			*path = append(*path, n)
-			maze[n.x][n.y] = black // Mark as visited.
-			if pathExists(maze, n, e, path) {
+	for _, shift := range []Coordinate{right, left, down, up} {
+		next := Coordinate{curr.x + shift.x, curr.y + shift.y}
+		if isFeasible(maze, next) { // Check if we can move this way.
+			*path = append(*path, next)
+			maze[next.x][next.y] = black // Mark as visited.
+			if pathExists(maze, next, end, path) {
 				return true
 			}
 			*path = (*path)[:len(*path)-1] // e wasn't found, take step back and try another one.
@@ -30,17 +30,17 @@ func pathExists(maze [][]bool, c, e Coordinate, path *[]Coordinate) bool {
 	return false
 }
 
-// SearchMaze searches for a path from s to e in the maze and return this path if exists.
+// SearchMaze returns true if a path exists from start to end in the maze.
 // The time complexity is O(v+e) where v is the number of vertices and e is the
 // number of edges in maze. Vertex is represented by boolean value true and edge
 // connects two vertices. O(v) additional space is needed.
-func SearchMaze(maze [][]bool, s, e Coordinate) (path []Coordinate) {
-	if !isFeasible(maze, s) {
+func SearchMaze(maze [][]bool, start, end Coordinate) (path []Coordinate) {
+	if !isFeasible(maze, start) {
 		return path
 	}
-	path = append(path, s)
-	maze[s.x][s.y] = black // Mark as visited.
-	if !pathExists(maze, s, e, &path) {
+	path = append(path, start)
+	maze[start.x][start.y] = black // Mark as visited.
+	if !pathExists(maze, start, end, &path) {
 		return []Coordinate(nil)
 	}
 	return path
