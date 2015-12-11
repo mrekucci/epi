225f0c7e91151da1c4d70c9c7bfaa6098a54fb13
Peter Mrekaj <mrekucci@gmail.com>
2015-12-11 10:32:49 -0800

    Change variable names in graphs.FlipColor* functions
    This change improves core readability.

--- graphs/flipcolor.go
+++ graphs/flipcolor.go
@@ -11,10 +11,10 @@ package graphs
 func FlipColorDFS(a [][]bool, x, y int) {
 	color := a[x][y]
 	a[x][y] = !color
-	for _, s := range []Coordinate{right, left, down, up} {
-		nx, ny := x+s.x, y+s.y
-		if nx >= 0 && nx < len(a) && ny >= 0 && ny < len(a[nx]) && a[nx][ny] == color {
-			FlipColorDFS(a, nx, ny)
+	for _, shift := range []Coordinate{right, left, down, up} {
+		next := Coordinate{x + shift.x, y + shift.y}
+		if next.x >= 0 && next.x < len(a) && next.y >= 0 && next.y < len(a[next.x]) && a[next.x][next.y] == color {
+			FlipColorDFS(a, next.x, next.y)
 		}
 	}
 }
@@ -29,12 +29,12 @@ func FlipColorBFS(a [][]bool, x, y int) {
 	a[x][y] = !color
 	q = append(q, Coordinate{x, y})
 	for len(q) > 0 {
-		c := q[0]
-		for _, s := range []Coordinate{right, left, down, up} {
-			n := Coordinate{c.x + s.x, c.y + s.y}
-			if n.x >= 0 && n.x < len(a) && n.y >= 0 && n.y < len(a[n.x]) && a[n.x][n.y] == color {
-				a[n.x][n.y] = !a[n.x][n.y]
-				q = append(q, n)
+		curr := q[0]
+		for _, shift := range []Coordinate{right, left, down, up} {
+			next := Coordinate{curr.x + shift.x, curr.y + shift.y}
+			if next.x >= 0 && next.x < len(a) && next.y >= 0 && next.y < len(a[next.x]) && a[next.x][next.y] == color {
+				a[next.x][next.y] = !a[next.x][next.y]
+				q = append(q, next)
 			}
 		}
 		q = q[1:]
