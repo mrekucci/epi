d530d5ec250d2e3f6da321a3878998e3770a50f9
Peter Mrekaj <mrekucci@gmail.com>
2015-12-14 20:48:26 -0800

    Remove pcomputing and designproblems packages, add dynamicprog package

--- designproblems/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2015, Peter Mrekaj. All rights reserved.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE.txt file.
-
-// Package designproblems implements solutions for the problems described in the
-// chapter Design Problems of the book Elements of Programming Interview.
-package designproblems
--- pcomputing/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2015, Peter Mrekaj. All rights reserved.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE.txt file.
-
-// Package pcomputing implements solutions for the problems described in the
-// chapter Parallel Computing of the book Elements of Programming Interview.
-package pcomputing
