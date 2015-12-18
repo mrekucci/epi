d734313a15076bf107024fabb6d8f65f492a1622
Peter Mrekaj <mrekucci@gmail.com>
2015-12-18 15:24:13 -0800

    Add time/space compexity analysis for ptypes.ClosestInt function

--- ptypes/closestint.go
+++ ptypes/closestint.go
@@ -14,9 +14,11 @@ func IntWeight(x uint64) (w int) {
 }
 
 // ClosestInt returns integer closest to x with the same weight.
+// The time complexity is O(n), where n is the integer width.
+// The O(1) additional space is needed.
 func ClosestInt(x uint64) (ci uint64, ok bool) {
 	for i := uint(0); i < 63; i++ {
-		if (x>>i)&1 != (x >> (i + 1) & 1) {
+		if (x>>i)&1 != x>>(i+1)&1 {
 			x ^= 1<<i | 1<<(i+1)
 			return x, true
 		}
