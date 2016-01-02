89c1eb0421081989e3aeaf6b17c58a4003969d27
Peter Mrekaj <mrekucci@gmail.com>
2016-01-01 19:10:29 -0800

    Make recursion.PowerSetRec function more readable

--- recursion/powerset.go
+++ recursion/powerset.go
@@ -5,21 +5,20 @@
 package recursion
 
 // genPowerSet generates a power set of s[i:] into the ps.
-func genPowerSet(i int, s, branch []interface{}, ps *[][]interface{}) {
+func genPowerSet(i int, s, branch []interface{}, ps [][]interface{}) [][]interface{} {
 	if i == len(s) {
-		*ps = append(*ps, append([]interface{}(nil), branch...))
-	} else {
-		genPowerSet(i+1, s, append(branch, s[i]), ps) // Generate all subset that contain s[i].
-		genPowerSet(i+1, s, branch, ps)               // Generate all subset that do not contain s[i].
+		return append(ps, append([]interface{}(nil), branch...))
 	}
+	ps = genPowerSet(i+1, s, append(branch, s[i]), ps) // Generate all subset that contain s[i].
+	ps = genPowerSet(i+1, s, branch, ps)               // Generate all subset that do not contain s[i].
+	return ps
 }
 
 // PowerSetRec returns a power set of s (it uses recursion to generate the set).
 // The time complexity is O(n*(2**n)). The space complexity is O(2**n)
 // The returned boolean value is always true.
 func PowerSetRec(s []interface{}) (ps [][]interface{}, ok bool) {
-	genPowerSet(0, s, []interface{}(nil), &ps)
-	return ps, true
+	return genPowerSet(0, s, nil, nil), true
 }
 
 // PowerSetItr returns a power set of s (it uses mapping to integer bits to generate the set).
