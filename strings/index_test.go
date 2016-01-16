7a2f5760a82c4f922e7ae2652f8f9c541100dbff
Peter Mrekaj <mrekucci@gmail.com>
2016-01-16 11:43:37 -0800

    Speedup benchmark for strings.Index* functions

--- strings/index_test.go
+++ strings/index_test.go
@@ -48,24 +48,18 @@ func testIndexFn(t *testing.T, fn indexFn, fnName string) {
 func TestIndexNaive(t *testing.T) { testIndexFn(t, IndexNaive, "IndexNaive") }
 func TestIndexRK(t *testing.T)    { testIndexFn(t, IndexRK, "IndexRK") }
 
-func benchIndexFn(b *testing.B, size int, fn indexFn, fnName string) {
-	b.StopTimer()
+func benchIndexFn(b *testing.B, size int, fn indexFn) {
+	s := epiutil.RandStr(size, "☺ abcdefghijklmnopqrstuvwxyz 世界", rand.NewSource(int64(size)))
+	p := s[size/3 : size*2/3]
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		s := epiutil.RandStr(size, "☺ abcdefghijklmnopqrstuvwxyz 世界", rand.NewSource(int64(i)))
-		o, c := size/3, size*2/3
-		p := s[o:c]
-		b.StartTimer()
-		r := fn(s, p)
-		b.StopTimer()
-		if r != o {
-			b.Errorf("%s did not find the index of the substring", fnName)
-		}
+		fn(s, p)
 	}
 }
 
-func BenchmarkIndexNaive1e4(b *testing.B) { benchIndexFn(b, 1e4, IndexNaive, "IndexNaive") }
-func BenchmarkIndexRK1e4(b *testing.B)    { benchIndexFn(b, 1e4, IndexRK, "IndexRK") }
-func BenchmarkIndexNaive1e6(b *testing.B) { benchIndexFn(b, 1e6, IndexNaive, "IndexNaive") }
-func BenchmarkIndexRK1e6(b *testing.B)    { benchIndexFn(b, 1e6, IndexRK, "IndexRK") }
-func BenchmarkIndexNaive1e8(b *testing.B) { benchIndexFn(b, 1e8, IndexNaive, "IndexNaive") }
-func BenchmarkIndexRK1e8(b *testing.B)    { benchIndexFn(b, 1e8, IndexRK, "IndexRK") }
+func BenchmarkIndexNaive1e4(b *testing.B) { benchIndexFn(b, 1e4, IndexNaive) }
+func BenchmarkIndexRK1e4(b *testing.B)    { benchIndexFn(b, 1e4, IndexRK) }
+func BenchmarkIndexNaive1e6(b *testing.B) { benchIndexFn(b, 1e6, IndexNaive) }
+func BenchmarkIndexRK1e6(b *testing.B)    { benchIndexFn(b, 1e6, IndexRK) }
+func BenchmarkIndexNaive1e8(b *testing.B) { benchIndexFn(b, 1e8, IndexNaive) }
+func BenchmarkIndexRK1e8(b *testing.B)    { benchIndexFn(b, 1e8, IndexRK) }
