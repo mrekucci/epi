db127d0f4f2215e0cdbb97ea7f0062d31ced4e15
Peter Mrekaj <mrekucci@gmail.com>
2016-05-13 20:12:06 +0200

    Refactor recursion helper function to use function literal

--- graphs/maze.go
+++ graphs/maze.go
@@ -9,27 +9,6 @@ func isFeasible(maze [][]bool, c Coordinate) bool {
 	return c.x >= 0 && c.x < len(maze) && c.y >= 0 && c.y < len(maze[c.x]) && maze[c.x][c.y] == white
 }
 
-// pathExists returns true if path from curr to end exists in maze.
-// Function is recursive implementation of DFS.
-func pathExists(maze [][]bool, curr, end Coordinate, path *[]Coordinate) bool {
-	if curr == end { // Base case.
-		return true
-	}
-
-	for _, shift := range []Coordinate{right, left, down, up} {
-		next := Coordinate{curr.x + shift.x, curr.y + shift.y}
-		if isFeasible(maze, next) { // Check if we can move this way.
-			*path = append(*path, next)
-			maze[next.x][next.y] = black // Mark as visited.
-			if pathExists(maze, next, end, path) {
-				return true
-			}
-			*path = (*path)[:len(*path)-1] // e wasn't found, take step back and try another one.
-		}
-	}
-	return false
-}
-
 // SearchMaze returns true if a path exists from start to end in the maze.
 // The time complexity is O(v+e) where v is the number of vertices and e is the
 // number of edges in maze. Vertex is represented by boolean value true and edge
@@ -40,7 +19,30 @@ func SearchMaze(maze [][]bool, start, end Coordinate) (path []Coordinate) {
 	}
 	path = append(path, start)
 	maze[start.x][start.y] = black // Mark as visited.
-	if !pathExists(maze, start, end, &path) {
+
+	// pathExists returns true if path from curr to end exists in maze.
+	// Function is recursive implementation of DFS.
+	var pathExists func(maze [][]bool, curr, end Coordinate) bool
+	pathExists = func(maze [][]bool, curr, end Coordinate) bool {
+		if curr == end { // Base case.
+			return true
+		}
+
+		for _, shift := range []Coordinate{right, left, down, up} {
+			next := Coordinate{curr.x + shift.x, curr.y + shift.y}
+			if isFeasible(maze, next) { // Check if we can move this way.
+				path = append(path, next)
+				maze[next.x][next.y] = black // Mark as visited.
+				if pathExists(maze, next, end) {
+					return true
+				}
+				path = path[:len(path)-1] // End wasn't found, take step back and try another one.
+			}
+		}
+		return false
+	}
+
+	if !pathExists(maze, start, end) {
 		return nil
 	}
 	return path
