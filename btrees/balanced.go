3df046bc7c4da125cf9a03f5f18503192f634d57
Peter Mrekaj <mrekucci@gmail.com>
2016-05-13 19:27:29 +0200

    Integrate auxiliary function as function literal

--- btrees/balanced.go
+++ btrees/balanced.go
@@ -4,36 +4,34 @@
 
 package btrees
 
-// checkBalance returns the height and balance
-// information about the binary tree t.
-func checkBalance(t *BTree) (height int, isBalanced bool) {
-	if t == nil {
-		return -1, true // Base case.
-	}
-
-	// Postorder walk.
-	lh, lb := checkBalance(t.left)
-	if !lb {
-		return lh, lb
-	}
-	rh, rb := checkBalance(t.right)
-	if !rb {
-		return rh, rb
-	}
-
-	d := lh - rh
-	max := lh
-	if lh < rh {
-		max = rh
-	}
-	return max + 1, d <= 1 && d >= -1
-
-}
-
 // IsBalanced returns true if t is a balanced binary tree.
 // The time complexity is O(n). The O(h) additional space
 // is needed (where h is the height of the tree).
 func IsBalanced(t *BTree) bool {
+	var checkBalance func(t *BTree) (height int, isBalanced bool)
+	checkBalance = func(t *BTree) (height int, isBalanced bool) {
+		if t == nil {
+			return -1, true // Base case.
+		}
+
+		// Post-order walk.
+		lh, lb := checkBalance(t.left)
+		if !lb {
+			return lh, lb
+		}
+		rh, rb := checkBalance(t.right)
+		if !rb {
+			return rh, rb
+		}
+
+		d := lh - rh
+		max := lh
+		if lh < rh {
+			max = rh
+		}
+		return max + 1, d <= 1 && d >= -1
+
+	}
 	_, b := checkBalance(t)
 	return b
 }
