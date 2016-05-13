c36d5f13bf3f2764410d43d6e1ef244c8fe079b0
Peter Mrekaj <mrekucci@gmail.com>
2016-05-13 19:20:57 +0200

    Integrate auxiliary function as function literal

--- bstrees/property.go
+++ bstrees/property.go
@@ -9,22 +9,21 @@ const (
 	minInt = -maxInt - 1
 )
 
-// areKeysInRange checks if tree t satisfies the BST property.
-func areKeysInRange(t *BSTree, min, max int) bool {
-	if t == nil {
-		return true
-	}
-	e := t.Data.(int)
-	if e < min || e > max {
-		return false
-	}
-	return areKeysInRange(t.left, min, e) && areKeysInRange(t.right, e, max)
-}
-
 // IsBinaryTreeBST returns true if given tree doesn't violate the BST property.
 // The time complexity is O(n), and O(h) additional space is needed, where h is
 // the binary tree height.
 // Note: Only int keys are accepted.
 func IsBinaryTreeBST(tree *BSTree) bool {
+	var areKeysInRange func(t *BSTree, min, max int) bool
+	areKeysInRange = func(t *BSTree, min, max int) bool {
+		if t == nil {
+			return true
+		}
+		e := t.Data.(int)
+		if e < min || e > max {
+			return false
+		}
+		return areKeysInRange(t.left, min, e) && areKeysInRange(t.right, e, max)
+	}
 	return areKeysInRange(tree, minInt, maxInt)
 }
