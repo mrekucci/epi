47dcb6c8dca79f392cf2a0bd5df82d87a1decdc4
Peter Mrekaj <mrekucci@gmail.com>
2016-05-13 21:45:06 +0200

    Refactor recursion helper function to use function literal

--- strings/reverse.go
+++ strings/reverse.go
@@ -18,36 +18,34 @@ func ReverseItr(s string) string {
 	return string(r)
 }
 
-// reverseAux recursively call it self with auxiliary variables to reverse r.
-func reverseAux(r []rune, i, j int) {
-	if i < j {
-		r[i], r[j] = r[j], r[i]
-		reverseAux(r, i+1, j-1)
-	}
-}
-
 // ReverseRecAux returns reversed string s.
-// The time complexity is O(n).
+// The time and the space complexity is O(n).
 // Note: it uses recursive way with auxiliary variables to reverse the string.
 // Warning: may cause stack overflow for long strings.
 func ReverseRecAux(s string) string {
 	r := []rune(s)
-	reverseAux(r, 0, len(r)-1)
+	var reverse func(i, j int)
+	reverse = func(i, j int) {
+		if i < j {
+			r[i], r[j] = r[j], r[i]
+			reverse(i+1, j-1)
+		}
+	}
+	reverse(0, len(r)-1)
 	return string(r)
 }
 
-// reversePure recursively call it self to reverse r.
-func reversePure(r []rune) []rune {
-	if len(r) == 0 {
-		return r
-	}
-	return append(reversePure(r[1:]), r[0])
-}
-
 // ReverseRecPure returns reversed string s.
-// The time complexity is O(n).
+// The time and the space complexity is O(n).
 // Note: it uses pure recursive way to reverse the string.
 // Warning: may cause stack overflow for long strings.
 func ReverseRecPure(s string) string {
-	return string(reversePure([]rune(s)))
+	var reverse func(r []rune) []rune
+	reverse = func(r []rune) []rune {
+		if len(r) == 0 {
+			return r
+		}
+		return append(reverse(r[1:]), r[0])
+	}
+	return string(reverse([]rune(s)))
 }
